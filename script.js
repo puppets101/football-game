3afc04448892c9b592a001f11232c445612103df
Oscar Andersson <oscar@Oscars-Air.lan>
2020-10-27 17:37:27 +0100

    lot of going back and forth

--- script.js
+++ script.js
@@ -1,38 +1,104 @@
+// Get text- and optionbuttons element from the DOM
+const textElement = document.getElementById('text');
+const optionButtonsElement = document.getElementById('option-buttons');
+
 // Define state
 let currentScene = 0;
 const scenes = [
+
   {
-    text: `You find yourself in a football game and it's happends to be the World Cup final the year 2052. On your feets you see a pair of what looks like shoes but with cables instead of shoestrings going in to your legs. You see a green and a red button on this so called shoes and you decide to press one of them. Witch button do you press?`,
-    options: ['Press green button', 'Press red button'],
-    nextScene: [1, 0]
+    id: 1,
+    text: `You find yourself in a football game and it's happends to be the World Cup final the year 2052. On your feets you see a pair of what looks like shoes but with blinking cables instead of shoestrings going in to your legs. You see a green and a red button on this so called shoes and you decide to press one of them. Witch button do you press?`,
+    options: [
+      {
+        text: 'Press green button',
+        nextScene: 2
+      },
+      {
+        text: 'Press red button',
+      }
+    ]
   },
+
   {
     text: `You hear a swoosh comming from underneath you and you realize the football boots in year 2052 has a hover effect. You make a couple of moves and find, to your relief, that the boots are not too much of a challange. Simultaneously the football game is in motion and you decide to join the game, after all you were a football star at a young age. What is your first action?`,
-    options: ['Call for the ball', 'Talk to a guy wearing the same shirt'],
+    options: [
+      {
+        id: 2,
+        text: 'Call for the ball',
+        nextScene: 3
+      },
+      {
+        text: 'Get hold of a teammate'
+      }
+    ]
+  },
+
+  {
+    text: `The cables stops blinking and only a few seconds later a hovering stretcher comes for you, pick you up, and removing you from the field.`,
     nextScene: []
-  }
+  },
+
+  {
+    text: `No one seems to hear you and you realize the whole arena, which probably contains around 200.000 supporters, is not making a sound. You take of your helmet and with that the hovering shoes stops working. You fall to the ground and is carried out of the field by a hovering strecher.`,
+    nextScene: []
+  },
+
 ];
 
-// Inventory
-const inventory = [''];
+let state = {};
 
-// Run application 
-window.onload = showText;
-
-// Define the actions 
-function showText() {
-  const answer = prompt(scenes[currentScene].text);
-  handleUserChoise(answer);
+function startGame() {
+  state = {};
+  showScenes(1);
 }
 
-function handleUserChoise(answer) {
+
+// Define the actions 
+function presentScene(textNodeIndex) {
+  textElement.innerText = scenes[currentScene].text; 
+
+  while (optionButtonsElement.firstChild) {
+    optionButtonsElement.removeChild(optionButtonsElement.firstChild);
+  }
+
+  options.forEach(option => {
+    if (showOption(option)) {
+      const button = document.createElement('button');
+      button.innerText = option.text;
+      button.classList.add('btn');
+      addEventListener('click', () => selectOption(option));
+      optionButtonsElement.appendChild(button);
+    }
+  })
+}
+
+function showOption(option) {
+  return true;
+}
+
+function selectOption(option) {
+
+}
+
+startGame();
+
+/* function handleUserChoise(answer) {
   console.log(answer);
+
   if (answer === scenes[currentScene].options[0]) {
     currentScene = scenes[currentScene].nextScene[0];
   }
   if (answer === scenes[currentScene].options[1]) {
     currentScene = scenes[currentScene].nextScene[1];
   }
+  if (answer === scenes[currentScene].options[3]) {
+    currentScene = scenes[currentScene].nextScene[3];
+  }
+  if (answer === scenes[currentScene].options[4]) {
+    currentScene = scenes[currentScene].nextScene[4];
+  }
 
-  showText();
-}
+  presentScene();
+} */
+
