dd9f9b13497d4868d01bfceab7c175176bd3416c
Oscar Andersson <oscar@Oscars-Air.lan>
2020-10-28 16:14:25 +0100

    functions in play but hard to understand

--- script.js
+++ script.js
@@ -2,10 +2,137 @@
 const textElement = document.getElementById('text');
 const optionButtonsElement = document.getElementById('option-buttons');
 
-// Define state
-let currentScene = 0;
+// Start game
+function startGame() {
+  presentScene(1);
+}
+
+
+// Define the actions 
+function presentScene(textIndex) {
+  const sceneText = scenes.find(sceneText => sceneText.id === textIndex);
+  textElement.innerText = sceneText.text;
+  while (optionButtonsElement.firstChild) {
+    optionButtonsElement.removeChild(optionButtonsElement.firstChild);
+  }
+
+  sceneText.options.forEach(option => {
+    if(showOption(option)) {
+      const button = document.createElement('button');
+      button.innerText = option.text;
+      button.classList.add('btn');
+      button.addEventListener('click', () => selectOption(option));
+      optionButtonsElement.appendChild(button);
+    }
+  })
+}
+
+function showOption(option) {
+  return true;
+}
+
+function selectOption(option) {
+  const nextTextId = option.nextText;
+  if (nextTextId < 0) {
+    return startGame();
+  }
+  presentScene(nextTextId);
+}
+
+/* function handleUserChoise(answer) {
+  console.log(answer);
+
+  if (answer === scenes[currentScene].options[0]) {
+    currentScene = scenes[currentScene].nextScene[0];
+  }
+  if (answer === scenes[currentScene].options[1]) {
+    currentScene = scenes[currentScene].nextScene[1];
+  }
+  if (answer !== scenes[currentScene].options[0] || scenes[currentScene].options[1]) {
+    console('Please try again');
+  }
+
+  presentScene();
+} */
+
+
 const scenes = [
-  { // 0
+  {
+    id: 1,
+    text: `In the second half of a football match you find yourself still on the bench, and with only fiftheen minutes to play left, the manager comes up to you and tell you to start warming up.`,
+    options: [
+      {
+        text: `Do as he say and start warming`,
+        nextText: 2
+      },
+      {
+        text: `Tell him you don't feel like playing`,
+        nextText: 3
+      }
+    ]
+  },
+  {
+    id: 2,
+    text: `After a couple minutes of warming you get the call that you're going to play. Quickly you get ready and get on to the pitch. The game is at high pace and you struggling to get hold of the ball. Suddenly you get the ball and see a teammate calling you for the ball.`,
+    options: [
+      {
+        text: `Pass the ball to your teammate`,
+        nextText: 4
+      },
+      {
+        text: `Dribble past the defenders`,
+        nextText: 5
+      },
+    ]
+  },
+  {
+    id: 3,
+    text: `The manager swears and looks angry at you and tells you go shower. In the dressingroom after the game the manager tells you not to come to the next match.`
+  },
+  {
+    id: 4,
+    text: `The ball gets safetly to you teammate who's advancing on the wing. You see an oppertunity to get the ball back inside the opponents penalty area.`,
+    options: [
+      {
+        text: `Run inside the penalty area`,
+        nextText: 6
+      },
+      {
+        text: `Wait outside the penalty area`,
+        nextText: 4
+      },
+    ]
+  },
+  {
+    id: 5,
+    text: `You've overestimated your dribble skill and a opponent tackles you and takes the ball. The opponents quickly counterattacks and score and your team lose the game.`,
+  },
+  {
+    id: 6,
+    text: `You've made the run inside the penalty area when a beautiful cross comes from your teammate. You manage to get clear of the defenders and go for the ball.`,
+    options: [
+      {
+        text: `Jump and header the ball`,
+        nextText: 7
+      },
+      {
+        text: `Do a bicicleta`,
+        nextText: 4
+      },
+      {
+        text: `Dive and hope for a penalty`,
+        nextText: 4
+      }
+    ]
+  },
+  {
+    id: 7,
+    text: `You collide mid air with the opponent defender's head and you feel like your skull is broken. The medical team quickly get you on a strecher and you are out of the game.`,
+    nextText: -1
+  },
+
+
+  /* { // 0
     text: `You are playing football. Do you want to pass or shoot?`,
     options: ['pass', 'shoot'],
     nextScene: [1, 2]
@@ -68,44 +195,8 @@ const scenes = [
   },
   { // SUBBED
     text: `You got subbed of the game by the manager.`
-  }
-
-];
-
-// Run program on load
-window.onload = presentScene;
-
-
-// Define the actions 
-function presentScene() {
-  textElement.innerText = scenes[currentScene].text;
-  const answer = document.getElementById('option-buttons');
-  console.log(answer);
-  /* const answer = prompt(scenes[currentScene].text);
-  handleUserChoise(answer); */
-  
-  /* const button = document.createElement('button');
-  button.innerText = options[0];
-  button.classList.add('btn'); */
-}
-
-
-function handleUserChoise(answer) {
-  console.log(answer);
-
-  if (answer === scenes[currentScene].options[0]) {
-    currentScene = scenes[currentScene].nextScene[0];
-  }
-  if (answer === scenes[currentScene].options[1]) {
-    currentScene = scenes[currentScene].nextScene[1];
-  }
-/*   if (answer === scenes[currentScene].options[3]) {
-    currentScene = scenes[currentScene].nextScene[3];
-  }
-  if (answer === scenes[currentScene].options[4]) {
-    currentScene = scenes[currentScene].nextScene[4];
   } */
 
-  presentScene();
-}
+]
 
+startGame();
